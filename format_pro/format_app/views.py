43389620196aa8c79693e663550dfa378232f4aa
AfaqCargo <aliafaq5327@gmail.com>
2025-05-07 06:48:10 +0400

    Update views.py

--- format_pro/format_app/views.py
+++ format_pro/format_app/views.py
@@ -18,6 +18,8 @@ import random
 from django.views import View
 from django.db import transaction
 from decimal import Decimal, getcontext
+from django.core.management import call_command
+from django.contrib.auth.models import User
 
 # Home/Dashboard view
 def home(request):
